6d8e5e969029727d15d5cd3a78b73b856d42fa42
ivychin <b082134@stumail.ltu.edu.tw>
2024-04-29 16:28:24 +0800

    line

--- MyWeb/Program.cs
+++ MyWeb/Program.cs
@@ -4,6 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews()
     .AddNewtonsoftJson();
 
+builder.Services.AddSingleton<LineBotConfig, LineBotConfig>((s)=> new LineBotConfig
+{
+    channelSecret = builder.Configuration.GetSection("LineBot")["channelSecret"]!,
+    accessToken = builder.Configuration.GetSection("LineBot")["accessToken"]!,
+});
+ 
+builder.Services.AddSingleton<ILineService, LineService>();
+builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
