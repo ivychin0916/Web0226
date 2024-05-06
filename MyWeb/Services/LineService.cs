49edba7be35010f7ea7013657150a1fdd91882ef
ivychin0916 <B082134@stumail.ltu.edu.tw>
2024-05-06 17:27:48 +0800

    抽籤

--- MyWeb/Services/LineService.cs
+++ MyWeb/Services/LineService.cs
@@ -12,6 +12,7 @@ public class LineService : ILineService
     {
         var result = null as List<ISendMessage>;
 
+        /*
         if (message == "你好")
         {
             result = new List<ISendMessage>
@@ -30,8 +31,39 @@ public class LineService : ILineService
                 ,new ImageMessage("https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg","https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg")
             };
             return result;
-        }
-        
+        }*/
+        /*
+        string[] imageUrl = new string[]
+        {
+            "https://i.imgur.com/pQSopio.png",
+            "https://i.imgur.com/352dYTW.png",
+            "https://i.imgur.com/vofyUaW.png",
+            "https://i.imgur.com/uvFnBWr.png",
+            "https://i.imgur.com/e4HcD7U.png",
+            "https://i.imgur.com/lVrr8se.png",
+            "https://i.imgur.com/y5CWKp3.png",
+        };
+        */
+
+        Random rnd = new Random((int)DateTime.Now.TimeOfDay.TotalSeconds);
+        int index= rnd.Next(0, 7);
+        string[] imageUrl = new string[]
+        {
+            "https://i.imgur.com/pQSopio.png",
+            "https://i.imgur.com/352dYTW.png",
+            "https://i.imgur.com/vofyUaW.png",
+            "https://i.imgur.com/uvFnBWr.png",
+            "https://i.imgur.com/e4HcD7U.png",
+            "https://i.imgur.com/lVrr8se.png",
+            "https://i.imgur.com/y5CWKp3.png",
+        };
+
+        return new List<ISendMessage>
+        {
+            new ImageMessage(imageUrl[index], imageUrl[index], null)
+        };
+
+
         result = new List<ISendMessage>
         {
             new TextMessage($"Receive a text event message \nchannelId={channelId}  \nuserId={userId}")
